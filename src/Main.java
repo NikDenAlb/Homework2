4dfbd7f06913aeba76884570e14d85d06011d409
NuzGooL <ardeniaanderland@gmail.com>
2023-06-26 12:27:35 +0300

    completed work

--- src/Main.java
+++ src/Main.java
@@ -17,89 +17,91 @@ public class Main {
         task8();
     }
 
-    public static void task1 () {
+    public static void task1() {
         System.out.println("Задача 1");
-        var dog     = 8.0;
-        var cat     = 3.6;
-        var paper   = 763789;
-        System.out.print("dog = ");System.out.println(dog);
-        System.out.print("cat = ");System.out.println(cat);
-        System.out.print("paper = ");System.out.println(paper);
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("dog = " + dog);
+        System.out.println("cat = " + cat);
+        System.out.print("paper = " + paper);
     }
 
 
-    public static void task2 () {
+    public static void task2() {
         System.out.println("Задача 2");
-        var dog     = 8.0;
-        var cat     = 3.6;
-        var paper   = 763789;
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
         dog += 4;
         cat += 4;
         paper += 4;
-        System.out.print("dog = ");System.out.println(dog);
-        System.out.print("cat = ");System.out.println(cat);
-        System.out.print("paper = ");System.out.println(paper);
+        System.out.println("dog = " + dog);
+        System.out.println("cat = " + cat);
+        System.out.println("paper = " + paper);
     }
 
 
-    public static void task3 () {
+    public static void task3() {
         System.out.println("Задача 3");
-        var dog     = 8.0;
-        var cat     = 3.6;
-        var paper   = 763789;
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
         dog -= 3.5;
         cat -= 1.6;
         paper -= 7639;
-        System.out.print("dog = ");System.out.println(dog);
-        System.out.print("cat = ");System.out.println(cat);
-        System.out.print("paper = ");System.out.println(paper);
+        System.out.println("dog = " + dog);
+        System.out.println("cat = " + cat);
+        System.out.println("paper = " + paper);
+
     }
 
 
-    public static void task4 () {
+    public static void task4() {
         System.out.println("Задача 4");
-        var friend     = 19;
-        System.out.print("Инициализация: friend = ");System.out.println(friend);
+        var friend = 19;
+        System.out.println("Инициализация: friend = " + friend);
         friend += 2;
-        System.out.print("Увеличение на 2: friend = ");System.out.println(friend);
+        System.out.println("Увеличение на 2: friend = " + friend);
         friend /= 7;
-        System.out.print("Деление на 7: friend = ");System.out.println(friend);
+        System.out.println("Деление на 7: friend = " + friend);
+
     }
 
-    public static void task5 () {
+    public static void task5() {
         System.out.println("Задача 5");
-        var frog     = 3.5;
-        System.out.print("Инициализация: frog = ");System.out.println(frog);
+        var frog = 3.5;
+        System.out.println("Инициализация: frog = " + frog);
         frog *= 10;
-        System.out.print("Умножение на 10: frog = ");System.out.println(frog);
+        System.out.println("Умножение на 10: frog = " + frog);
         frog /= 3.5;
-        System.out.print("Деление на 3.5: frog = ");System.out.println(frog);
+        System.out.println("Деление на 3.5: frog = " + frog);
         frog += 4;
-        System.out.print("Увеличение на 4: frog = ");System.out.println(frog);
+        System.out.println("Увеличение на 4: frog = " + frog);
     }
 
-    public static void task6 () {
+    public static void task6() {
         System.out.println("Задача 6");
-        var aBoxerWeight= 78.2;
-        var bBoxerWeight= 82.7;
+        var aBoxerWeight = 78.2;
+        var bBoxerWeight = 82.7;
 
         var totalWeight = aBoxerWeight + bBoxerWeight;
-        System.out.println("Общий вес двух бойцов "+totalWeight+" кг");
+        System.out.println("Общий вес двух бойцов " + totalWeight + " кг");
 
         var weightDifference = aBoxerWeight - bBoxerWeight;
-        System.out.println("Вес боксёра A больше веса боксёра B на "+weightDifference+" кг");
+        System.out.println("Вес боксёра A больше веса боксёра B на " + weightDifference + " кг");
     }
 
-    public static void task7 () {
+    public static void task7() {
         System.out.println("Задача 7");
-        var aBoxerWeight= 78.2;
-        var bBoxerWeight= 82.7;
+        var aBoxerWeight = 78.2;
+        var bBoxerWeight = 82.7;
 
-        var plusWeightDifference =  bBoxerWeight - aBoxerWeight;
-        System.out.println("Разница в весе через вычитание из большего: "+plusWeightDifference+" кг");
+        var plusWeightDifference = bBoxerWeight - aBoxerWeight;
+        System.out.println("Разница в весе через вычитание из большего: " + plusWeightDifference + " кг");
 
-        var plusModuloWeightDifference =  bBoxerWeight % aBoxerWeight;
-        System.out.println("Разница в весе через функцию остаток от деления на большее: "+plusModuloWeightDifference+" кг");
+        var plusModuloWeightDifference = bBoxerWeight % aBoxerWeight;
+        System.out.println("Разница в весе через функцию остаток от деления на большее: " + plusModuloWeightDifference + " кг");
 
         /*От задания осталось негативное впечатление. Пока не известен инструментарий определить какое значение больше.
         Прошлая задача при переходе к корявому языку позволяла не потерять эти данные.
@@ -108,18 +110,18 @@ public class Main {
     }
 
 
-    public static void task8 () {
+    public static void task8() {
         System.out.println("Задача 8");
-        var totalWorkTime   = 640;
-        var workingHours    = 8;
+        var totalWorkTime = 640;
+        var workingHours = 8;
 
-        var numberEmployees =  totalWorkTime / workingHours;
-        System.out.println("Всего работников в компании — "+numberEmployees+" человек");
+        var numberEmployees = totalWorkTime / workingHours;
+        System.out.println("Всего работников в компании — " + numberEmployees + " человек");
 
         var addNubmberEployees = 94;
         var imagineNumberEmployees = numberEmployees + addNubmberEployees;
-        System.out.println("Если в компании работает "+imagineNumberEmployees+" человек, то всего "
-                +imagineNumberEmployees*workingHours+" часов работы может быть поделено между сотрудниками");
+        var imagineTotalWorkTime = imagineNumberEmployees * workingHours;
+        System.out.println("Если в компании работает " + imagineNumberEmployees + " человек, то всего " + imagineTotalWorkTime + " часов работы может быть поделено между сотрудниками");
 
 
         /*Задача крайне не логична. Количество часов не может быть так просто поделено между сотрудниками, так как невозможно
